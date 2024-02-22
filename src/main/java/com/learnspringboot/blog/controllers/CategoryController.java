bc5131a16a839a5fc384e2ed65b27b6fd5658f9c
Arvind <arvinkum20@gmail.com>
2024-02-22 17:44:53 +0530

    dfg

--- src/main/java/com/learnspringboot/blog/controllers/CategoryController.java
+++ src/main/java/com/learnspringboot/blog/controllers/CategoryController.java
@@ -5,7 +5,7 @@ import com.learnspringboot.blog.services.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
